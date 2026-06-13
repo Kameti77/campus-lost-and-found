02f2ba5b06053a374b9a47bbfdb3bbaa34d1db57
Kameti <kametidanielk@gmail.com>
2026-06-13 16:44:16 -0400

    update API URLs for production

--- lost-and-found-backend/src/server.js
+++ lost-and-found-backend/src/server.js
@@ -6,7 +6,9 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: "https://lost-and-found.onrender.com"
+}))
 app.use(express.json());
 
 const path = require("path");
